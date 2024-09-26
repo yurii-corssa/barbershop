b14a1e68ca437c4d8b79b0ce6d21de17cbb00357
yurii-corssa <yurii.corssa@gmail.com>
2024-09-26 20:39:16 +0200

    optimize event listeners

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,13 +3,18 @@ const closeBtn = document.querySelector(".mobile-menu__close-btn-js");
 const mobileMenu = document.querySelector(".mobile-menu-js");
 const backdrop = document.querySelector(".backdrop-js");
 const body = document.body;
-const navLinks = document.querySelectorAll(".mobile-menu__link-js");
+const navLinks = document.querySelector(".nav-links-container-js");
 
-const openMenu = () => {
-  mobileMenu.classList.add("visible");
-  backdrop.classList.add("visible");
-  body.classList.add("no-scroll");
-  mobileMenu.setAttribute("aria-hidden", "false");
+const handleKeyDown = e => {
+  if (e.key === "Escape") {
+    closeMenu();
+  }
+};
+
+const handleClickLinks = e => {
+  if (e.target.classList.contains("mobile-menu__link-js")) {
+    closeMenu();
+  }
 };
 
 export const closeMenu = () => {
@@ -17,15 +22,23 @@ export const closeMenu = () => {
   backdrop.classList.remove("visible");
   body.classList.remove("no-scroll");
   mobileMenu.setAttribute("aria-hidden", "true");
+
+  closeBtn.removeEventListener("click", closeMenu);
+  backdrop.removeEventListener("click", closeMenu);
+  document.removeEventListener("keydown", handleKeyDown);
+  navLinks.removeEventListener("click", handleClickLinks);
+};
+
+const openMenu = () => {
+  mobileMenu.classList.add("visible");
+  backdrop.classList.add("visible");
+  body.classList.add("no-scroll");
+  mobileMenu.setAttribute("aria-hidden", "false");
+
+  closeBtn.addEventListener("click", closeMenu);
+  backdrop.addEventListener("click", closeMenu);
+  document.addEventListener("keydown", handleKeyDown);
+  navLinks.addEventListener("click", handleClickLinks);
 };
 
 burgerBtn.addEventListener("click", openMenu);
-
-closeBtn.addEventListener("click", closeMenu);
-backdrop.addEventListener("click", closeMenu);
-document.addEventListener("keydown", e => {
-  if (e.key === "Escape") {
-    closeMenu();
-  }
-});
-navLinks.forEach(link => link.addEventListener("click", closeMenu));
